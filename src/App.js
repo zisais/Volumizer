47d7e945fe337b0677d1d1c22f77a60792c4fb94
zisais <118950928+zisais@users.noreply.github.com>
2024-03-14 22:05:03 -0500

    Update index.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Volumizer from "./volumizer";
 function App() {
   return (
     <div className="App">
-      <head>
-        <title>Volumizer</title>
-      </head>
       <Volumizer/>
     </div>
   );
