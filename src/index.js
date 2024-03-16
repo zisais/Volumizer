515bd9411dfd0962d2affa2cc2e881bba829e8ed
zisais <118950928+zisais@users.noreply.github.com>
2024-03-15 21:32:34 -0500

    add Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <title>Volumizer</title>
   </head>
   <App />
+  <Analytics/>
   </React.StrictMode>
 );
 
